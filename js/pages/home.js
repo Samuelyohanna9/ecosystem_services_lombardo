3a768d51a54134427d11edb829d105148bd7551a
Samuelyohanna9 <samuelyohanna9@gmail.com>
2025-11-26 17:33:57 +0100

    update

--- js/pages/home.js
+++
@@ -1,4 +0,0 @@
-export function initHomePage(){
-  // for now it's static
-  // later you can fetch announcements/stats here
-}
--- js/pages/trees.js
+++
@@ -1,4 +0,0 @@
-export function initTreesPage(){
-  // placeholder module
-  // later: load tree dashboards, charts, filters, etc.
-}
